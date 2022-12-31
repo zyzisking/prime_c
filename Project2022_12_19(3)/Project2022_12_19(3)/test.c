254b980ff6983218fae446360c18b0c3e98fe43e
zyzisking <zyz15841609389@126.com>
2022-12-31 16:27:02 +0800

    2022_12_31牛客网编程初学者入门训练

---
+++ Project2022_12_19(3)/Project2022_12_19(3)/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main()
+{
+    printf("The size of short is %d bytes.\n", sizeof(short));
+    printf("The size of int is %d bytes.\n", sizeof(int));
+    printf("The size of long is %d bytes.\n", sizeof(long));
+    printf("The size of long long is %d bytes.\n", sizeof(long long));
+    return 0;
+}
---
+++ Project2022_12_19(5)/Project2022_12_19(5)/test.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%#o ", 1234);
+    printf("%#X\n", 1234);
+}
